2338008988b883e632af53695377f114097beddb
fedorovd <danil.fedorov@aisnovations.com>
2021-08-11 13:11:53 +0300

    .

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import pkg from './package.json';
 
 export default [
   {
-    input: 'src/action-creator.ts',
+    input: 'src/index.ts',
     external: Object.keys(pkg.peerDependencies || {}),
     plugins: [
       typescript({
