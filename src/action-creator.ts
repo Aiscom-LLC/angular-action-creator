61a92eddc420420a3572656c7613a8a6be0be833
vsevolod gandzyuck <v.gandzyuck@aisnovations.com>
2021-08-12 10:42:30 +0300

    Return types

--- src/action-creator.ts
+++ src/action-creator.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from '@ngrx/store';
+import { TypedAction } from '@ngrx/store/src/models';
 import { ErrorState, ActionCreatorProps } from './interfaces';
 
 export class ActionCreator {
@@ -7,11 +8,15 @@ export class ActionCreator {
     this.prefix = `[${prefix}]`;
   }
 
-  createAction<T extends object>(type: string) {
+  createAction<T extends Record<string, any>>(type: string): TypedAction<string> {
     return createAction(`${this.prefix} ${type}`, props<T>() as ActionCreatorProps<any>);
   }
 
-  createAsyncAction<Q extends object, W extends object, E extends object = ErrorState>(type: string) {
+  createAsyncAction<Q extends Record<string, any>, W extends Record<string, any>, E extends Record<string, any> = ErrorState>(type: string): {
+    started: TypedAction<string>,
+    success: TypedAction<string>,
+    failed: TypedAction<string>
+  } {
     return {
       started: createAction(`${this.prefix} ${type}_STARTED`, props<Q>() as ActionCreatorProps<any>),
       success: createAction(`${this.prefix} ${type}_SUCCESS`, props<W>() as ActionCreatorProps<any>),
