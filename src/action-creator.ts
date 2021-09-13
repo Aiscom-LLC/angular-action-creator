5c1edac2d56dfbc9cc49cd98cf7d5554101d37d2
vsevolod gandzyuck <v.gandzyuck@aisnovations.com>
2021-09-13 11:13:40 +0300

    Types are not required

--- src/action-creator.ts
+++ src/action-creator.ts
@@ -11,7 +11,7 @@ export class ActionCreator {
     return createAction(`${this.prefix} ${type}`, props<T>() as ActionCreatorProps<any>) as SyncActionType<T>;
   }
 
-  createAsyncAction<Q extends Record<string, any>, W extends Record<string, any>, E extends Record<string, any> = ErrorState>(type: string): AsyncActionType<Q, W, E> {
+  createAsyncAction<Q extends Record<string, any> = {}, W extends Record<string, any> = {}, E extends Record<string, any> = ErrorState>(type: string): AsyncActionType<Q, W, E> {
     return {
       started: createAction(`${this.prefix} ${type}_STARTED`, props<Q>() as ActionCreatorProps<any>) as SyncActionType<Q>,
       success: createAction(`${this.prefix} ${type}_SUCCESS`, props<W>() as ActionCreatorProps<any>) as SyncActionType<W>,
